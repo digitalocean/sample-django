36f0dac5766fe65df3d99e7eef0b0bd4d2af4082
Mason Egger <mason@masonegger.com>
2020-10-08 09:27:37 -0500

    adding frontpage

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
+    path("", include("frontpage.urls")),
 ]
